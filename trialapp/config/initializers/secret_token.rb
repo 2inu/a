9f7bf16889a5a50e75845219d82035ed3b8c1803
2inu <2inuuu@gmail.com>
2012-01-20 09:55:58 +0530

    the very first commit

---
+++ trialapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trialapp::Application.config.secret_token = 'e398c4e29e349931a29638dcf737a4ad3e15059409a779d465f3010cecf38af99a435d585308ddc67a686d513afac5875f2d9d358f9d13ee5730ffece52a9e3c'
